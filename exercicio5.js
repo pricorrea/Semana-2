94117804c329b029f42cff76c56e22e98f170be8
Priscila Correa <pdepri@gmail.com>
2021-05-21 22:33:56 -0300

    Inclusão Exercicios5

---
+++ exercicio5.js
@@ -0,0 +1,11 @@
+// 5 - Receba do usuários 3 números e calcule a média entre eles.
+
+let numero1 = 32
+
+let numero2 = 65 
+
+let numero3 = 143
+
+let media = (numero1 + numero2 + numero3)/3
+
+console.log(`A média é igual a ${media}.`)
