164112fd6c1f16cb9b85d9177f14be315d93a5a5
Priscila Correa <pdepri@gmail.com>
2021-05-21 22:27:24 -0300

    Incusão Exercicio2

---
+++ exercicio2.js
@@ -0,0 +1,9 @@
+//2 - Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distanciaKm = 200
+
+let velocidadeMediaKmH = 80
+
+let tempoDeViagem = distanciaKm / velocidadeMediaKmH
+
+console.log(`O tempo de viagem de carro durou ${tempoDeViagem} horas`)
